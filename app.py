85e23338689c48bdb91f14bdd0f40e41d2ee1452
SambhavK11 <sambhavk11@gmail.com>
2021-07-18 14:34:31 +0800

    Basic currency converter

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, make_response
 import json
+import requests
 
 
 app = Flask(__name__)
@@ -27,9 +28,16 @@ def processRequest(req):
     
     number1=1
     number2 =4
-    fulfilmentres="the sum is : " + str(number1+number2)
+    URL="https://free.currconv.com/api/v7/convert"
+    PARAMS={"q":"SGD_INR","compact":"ultra","apiKey":"7a2db6e45a77e71a05c9"}
+    r=requests.get(url=URL,params=PARAMS)
+    data=r.json()
+    fulfilmentres="the currency conversion for now is " + str(data['SGD_INR'])
     return {"fulfillmentText":fulfilmentres}
 
 if __name__ == '__main__':
     app.run()
 
+
+    
+"""https://free.currconv.com/api/v7/convert?q=USD_SGD&compact=ultra&apiKey=7a2db6e45a77e71a05c9"""
